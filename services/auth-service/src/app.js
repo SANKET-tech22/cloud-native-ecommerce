656f3c9898b8cfa65f84720d09b3d4f24928adf6
Sanket-tech22 <sanketpatil08127@gmail.com>
2026-04-07 13:19:19 +0530

    Test CICD Pipeline

--- services/auth-service/src/app.js
+++ services/auth-service/src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "Auth Service Running" });
+  res.status(200).json({ status: "Auth Service Running v2 🚀" });
 });
 
 const PORT = process.env.PORT || 5000;
